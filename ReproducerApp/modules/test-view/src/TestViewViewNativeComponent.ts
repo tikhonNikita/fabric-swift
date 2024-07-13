2e87b553d8e989634480c6bf6ba73707ec481611
Nikita Tikhonov <jagarnikita@gmail.com>
2024-07-13 14:49:46 +0200

    update codegenNativeComponent params

--- ReproducerApp/modules/test-view/src/TestViewViewNativeComponent.ts
+++ ReproducerApp/modules/test-view/src/TestViewViewNativeComponent.ts
@@ -5,4 +5,6 @@ interface NativeProps extends ViewProps {
   color?: string;
 }
 
-export default codegenNativeComponent<NativeProps>('TestViewView');
+export default codegenNativeComponent<NativeProps>('TestViewView', {
+  interfaceOnly: true,
+});
